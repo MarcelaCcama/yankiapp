5cf4d3b9b501eef3e405220bedf687ccf6f72547
MarcelaCcama <marcelaccama2809@gmail.com>
2023-09-04 06:59:46 -0500

    modificado configuracion de swagger en 'app.yaml' y 'gatewayservice.yaml' para el proyecto para activar swagger

--- app/src/main/java/com/yanki/app/AppApplication.java
+++ app/src/main/java/com/yanki/app/AppApplication.java
@@ -2,13 +2,17 @@ package com.yanki.app;
 
 import com.netflix.discovery.EurekaNamespace;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@OpenAPIDefinition
+
 @EnableFeignClients
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(title = "Employee API", version = "1.0", description = "Documentation Employee API v1.0")
+)
 public class AppApplication {
 
 	public static void main(String[] args) {
