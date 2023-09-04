766f0e8506ff1c87b35e582f654f64af42c5a77b
MarcelaCcama <marcelaccama2809@gmail.com>
2023-09-03 21:21:09 -0500

    agregando configuracion de swagger en 'app.yaml' para el proyecto 'app'

--- app/src/main/java/com/yanki/app/AppApplication.java
+++ app/src/main/java/com/yanki/app/AppApplication.java
@@ -1,11 +1,12 @@
 package com.yanki.app;
 
 import com.netflix.discovery.EurekaNamespace;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-
+@OpenAPIDefinition
 @EnableFeignClients
 @SpringBootApplication
 public class AppApplication {
